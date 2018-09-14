0f4816d9fce1edc69c3cf90d03692b3bfcb12f03
Karl Pena <karl@scientiamobile.com>
2018-09-14 12:45:16 -0400

    Adding basic files

---
+++ aws.tf
@@ -0,0 +1,34 @@
+# Configure the AWS Provider
+
+# Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "ELK-www"
+provider "aws" {
+  region                  = "us-east-2"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "personal"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "ELK-www"
+  }
+}
