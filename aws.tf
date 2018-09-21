56dd74b6879cb58ae01380fd4f9622043ec4a1ce
Karl Pena <karl@scientiamobile.com>
2018-09-21 17:16:36 -0400

    Formatting updates

--- aws.tf
+++ aws.tf
@@ -1,7 +1,9 @@
 # Configure the AWS Provider 
 
 # Create a new instance of the latest Ubuntu 14.04 on an
-# t2.micro node with an AWS Tag naming it "ELK-www"
+# t2.micro node with an AWS Tag naming it "www-01"
+# Ubuntu AMI ID = ami-0782e9ee97725263d
+
 provider "aws" {
   region                  = "us-east-2"
   shared_credentials_file = "~/.aws/credentials"
@@ -12,7 +14,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
+    name = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
   }
 
@@ -25,10 +27,58 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  #  ami           = "${data.aws_ami.ubuntu.id}"
+  ami           = "ami-0782e9ee97725263d"
   instance_type = "t2.micro"
 
   tags {
-    Name = "ELK-www"
+    Name = "www-01"
   }
+
+
+
+
+  ### ENABLE TO DEBUG CONFIGURAITON:
+
+  #output "lb_address" {
+  #  value = "${aws_alb.web.public_dns}"
+  #}
+
+  #  output "instance_ip" {
+  #    value = ["${aws_instance.web.0.public_ip}"]
+  #  }
+
+  #resource "aws_security_group" "sg-www" {
+  #  name = "sg-www"
+  #  description = "default VPC security group"
+  #
+  #  # TCP access
+  #    ingress {
+  ##    from_port = 0
+  ##    to_port = 65535
+  #from_port = 22
+  #to_port = 80
+  #    protocol = "tcp"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
+  #
+  #  # HTTP access from anywhere
+  #  ingress {
+  #    from_port = 80
+  #    to_port = 80
+  #    protocol = "tcp"
+  #    cidr_blocks = ["0.0.0.0/0"]
+  #  }
+  #}
+
+  #  resource "aws_iam_user" "user" {
+  #    name = "${var.aws_iam_user}"
+  #    path = "/"
+  #  }
+  #
+  #    resource "aws_iam_user_ssh_key" "username" {
+  #      username   = "${var.aws_ssh_user}"
+  #      encoding   = "SSH"
+  #      public_key = "${var.ssh_public_key}"
+  #    }
 }
